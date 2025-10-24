3c61011f6ce93f092f1f93befcc4bcd7693404af
livia.almeida <livia.almeida@ccc.ufcg.edu.br>
2025-10-24 15:32:46 -0300

    feat: adiciona validacao de whatsapp

--- script.js
+++ script.js
@@ -1,18 +1,47 @@
 const NUMERO_DESTINO = '5583996000281'; // WhatsApp da empresa
 
-function validarWhats(numero) {
-  const limpo = numero.replace(/\D/g, '');
-  const regex = /^(?:\+?55)?(?:[1-9][0-9])9[0-9]{8}$/;
-  return regex.test(limpo);
+async function avaliarNumero(numero) {
+  const url = `https://whatsapp-checker.p.rapidapi.com/check?phone=%2B55${numero}`;
+  const options = {
+    method: 'GET',
+    headers: {
+      'x-rapidapi-key': '960aec5688mshce87f4beeeed823p11a6fejsnc81fecd27042',
+      'x-rapidapi-host': 'whatsapp-checker.p.rapidapi.com'
+    }
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const result = await response.json();
+    return result[0].exists;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-document.getElementById('leadForm').addEventListener('submit', function(e) {
+async function validarWhats(numero) {
+  const limpo = numero.replace(/\D/g, '');
+  const regex = /^(?:\+?55)?(?:[1-9][0-9])9[0-9]{8}$/;
+  //return regex.test(limpo);
+
+    // 1.Validar formatação do número  
+  const isNumeroFormatado = regex.test(limpo);
+
+  // 2. Validar existência do número via API
+  let isNumeroValido = await avaliarNumero(limpo);
+
+  return isNumeroFormatado && isNumeroValido;
+}
+
+document.getElementById('leadForm').addEventListener('submit', async function(e) {
   e.preventDefault();
   const nome = document.getElementById('nome').value;
   const whats = document.getElementById('whats').value;
   const erro = document.getElementById('erro');
 
-  if (!validarWhats(whats)) {
+  const isNumeroValido = await validarWhats(whats);
+
+  if (!isNumeroValido) {
     erro.textContent = 'Número inválido! Informe um WhatsApp válido com DDD.';
     return;
   }
